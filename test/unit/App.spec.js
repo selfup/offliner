a0dc8ea8fb20a7f5967bcc9f043fafe486f757de
selfup <boudinot.regis@yahoo.com>
2016-09-08 23:25:46 -0600

    testing dispatch methods - getting a good grasp on this

--- test/unit/App.spec.js
+++ test/unit/App.spec.js
@@ -14,7 +14,7 @@ describe('App.vue', () => {
   })
 
   const entryPoint = document.createElement('div')
-  entryPoint.setAttribute('id', 'app')
+        entryPoint.setAttribute('id', 'app')
 
   const vm = new Vue({
     el: entryPoint,
@@ -29,5 +29,18 @@ describe('App.vue', () => {
     expect(vm.$options.store.state.body).toBe('')
     expect(vm.$options.store.state.searched).toBe(false)
     expect(typeof vm.$options.store.state.helper).toBe('object')
+  })
+
+  it('can add an idea using vuex', () => {
+    vm.$options.store.dispatch('UPDATE_TITLE', 'hello')
+    expect(vm.$options.store.state.title).toBe('hello')
+
+    vm.$options.store.dispatch('UPDATE_BODY', 'world')
+    expect(vm.$options.store.state.body).toBe('world')
+
+    vm.$options.store.dispatch('ADD_IDEA')
+    expect(vm.$options.store.state.ideas[0].title).toBe('hello')
+    expect(vm.$options.store.state.ideas[0].body).toBe('world')
+    expect(vm.$options.store.state.ideas[0].quality).toBe('Swill')
   })
 })
